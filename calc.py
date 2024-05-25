67c6e7694de400c3356f46ca07c1ffddbadbaedb
Yuriy <yurakirin9545@gmail.com>
2024-05-25 05:20:39 +0300

    added dist

--- calc.py
+++ calc.py
@@ -1,49 +1,34 @@
 import tkinter as tk
 import math
 
-
 def plus(a, b):
     return float(a + b)
-
 
 def minus(a, b):
     return float(a - b)
 
-
 def mul(a, b):
     return float(a * b)
-
 
 def div(a, b):
     return float(a / b)
 
-
 def sin(a):
     return math.sin(a)
-
 
 def cos(a):
     return math.cos(a)
 
-
 def floor(a):
     return math.floor(a)
-
 
 def ceil(a):
     return math.ceil(a)
 
-
 def mod(a, b):
     return a % b
 
-
-root = tk.Tk()
-root.title("Калькулятор")
-root.geometry("400x200")
-
-
-def calculate():
+def calculate(entry1, entry2, dropdown, result_label):
     try:
         num1 = float(entry1.get())
         operation = dropdown.get()
@@ -84,8 +69,7 @@ def calculate():
     except ValueError:
         result_label.config(text="Непредвиденная ошибка")
 
-
-def on_dropdown_change(*args):
+def on_dropdown_change(entry2, dropdown):
     operation = dropdown.get()
     if operation in ['sin', 'cos', 'floor', 'ceil']:
         entry2.delete(0, tk.END)
@@ -93,23 +77,27 @@ def on_dropdown_change(*args):
     else:
         entry2.config(state='normal')
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Калькулятор")
+    root.geometry("400x200")
 
-entry1 = tk.Entry(root, width=20)
-entry1.pack()
-entry2 = tk.Entry(root, width=20)
-entry2.pack()
+    entry1 = tk.Entry(root, width=20)
+    entry1.pack()
+    entry2 = tk.Entry(root, width=20)
+    entry2.pack()
 
-dropdown = tk.StringVar(root)
-dropdown.set('+')
-options = ['+', '-', '*', '/', 'sin', 'cos', 'floor', 'ceil', 'mod']
-dropdown.trace('w', on_dropdown_change)
-dropdown_menu = tk.OptionMenu(root, dropdown, *options)
-dropdown_menu.pack()
+    dropdown = tk.StringVar(root)
+    dropdown.set('+')
+    options = ['+', '-', '*', '/', 'sin', 'cos', 'floor', 'ceil', 'mod']
+    dropdown.trace('w', lambda *args: on_dropdown_change(entry2, dropdown))
+    dropdown_menu = tk.OptionMenu(root, dropdown, *options)
+    dropdown_menu.pack()
 
-calculate_button = tk.Button(root, text="Calculate", command=calculate)
-calculate_button.pack()
+    calculate_button = tk.Button(root, text="Calculate", command=lambda: calculate(entry1, entry2, dropdown, result_label))
+    calculate_button.pack()
 
-result_label = tk.Label(root, text="Result:")
-result_label.pack()
+    result_label = tk.Label(root, text="Result:")
+    result_label.pack()
 
-root.mainloop()
+    root.mainloop()
